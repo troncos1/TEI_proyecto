46432520d5208cb95de67008cc563ca29cf28f4d
Jorge Ignacio Moreno Farías <116883432+troncos1@users.noreply.github.com>
2024-11-04 12:21:08 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,6 @@ export default function Home() {
       }}
     >
       <div className="absolute top-0 left-0 m-4 md:left-0 md:top-0 md:m-4 flex justify-center md:justify-start w-full">
-        <img src="/Logo-UAI-1-scaled.svg" alt="UAI Logo" width={150} height={150} />
-      </div>
-      <div
-        className="p-8 rounded-lg shadow-lg max-w-md w-full text-center"
-        style={{
-          background: "rgba(0, 0, 0, 0.7)", 
-          boxShadow: "0 8px 32px 0 rgba(0, 0, 0, 0.5)",
-          backdropFilter: "blur(10px)",
-          WebkitBackdropFilter: "blur(10px)",
-          borderRadius: "10px",
-          border: "1px solid rgba(255, 255, 255, 0.25)", 
-        }}
-      >
         <h1 className="text-2xl font-bold mb-6 text-white">Welcome to UAI</h1>
         <div className="flex flex-col space-y-4">
           <Link href="/password/login">
@@ -38,23 +25,10 @@ export default function Home() {
                 color: "white",
               }}
             >
-              Student
+              Login
             </p>
           </Link>
-          <Link href="/password/login2">
-            <p
-              className="font-bold py-2 px-4 rounded hover:bg-white"
-              style={{
-                background: "rgba(255, 255, 255, 0.2)",
-                boxShadow: "0 4px 16px 0 rgba(0, 0, 0, 0.25)",
-                backdropFilter: "blur(5px)",
-                WebkitBackdropFilter: "blur(5px)",
-                border: "1px solid rgba(255, 255, 255, 0.5)",
-                color: "white",
-              }}
-            >
-              Professor
-            </p>
+
           </Link>
         </div>
       </div>
